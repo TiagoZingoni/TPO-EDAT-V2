ab318d939431d10457cae1be52013bc46ec7b37a
TiagoZingoni <tiagozngi@gmail.com>
2025-12-08 23:45:28 -0300

    falta funcion hash

--- TDAs/MapeoAUno.java
+++ TDAs/MapeoAUno.java
@@ -1,37 +1,131 @@
 package TDAs;
-import Mudanzas.ClaveCliente;
+
 // MapeoAUno Hash Abierto
 public class MapeoAUno {
     private int TAMANO; //Constante del limite
-    private NodoHashMapeo[] lista;
+    private NodoHashMapeo[] tabla;
     private int cant;
 
     public MapeoAUno(int tamanoLista){
         this.TAMANO = tamanoLista;
-        this.lista = new NodoHashMapeo[TAMANO-1];
+        this.tabla = new NodoHashMapeo[TAMANO-1];
         this.cant = 0;
     }
 
-    public boolean asociar(Object ClaveCliente, Object datosCliente){
+    public boolean asociar(Object dominio, Object rango){
         /* Recibe un valor que representa a un elemento del dominio y un segundo valor que representa a
         un elemento del rango. Si no existe otro par que contenga a valorDominio, agrega en el mapeo el 
         par (valorDominio, valorRango). Si la operación termina con éxito devuelve verdadero y falso en
         caso contrario.*/
-        boolean asociado = false;
-        for(int i = 0; i<TAMANO; i++){
-            
+        int posicion = funcionHash(dominio); //lugar donde iría el nuevo par
+        boolean asociado = true;
+        NodoHashMapeo actual = tabla[posicion]; //Nodo que estamos evaluando actualmente
+
+        while(actual!=null && asociado){
+            // Buscamos que el dominio no exista, así como agregarlo al final de la "lista" de objetos
+            if(actual.getDominio().equals(dominio)){
+                asociado = false;
+            }
+            actual = actual.getEnlace();
         }
-        return asociado;
+        if(asociado){
+            // Si el nodo no existía lo agrego como primero
+            tabla[posicion] = new NodoHashMapeo(dominio,rango,tabla[posicion]);
+            cant++;// +1 en cantidad de objetos en la tabla si se agrego
+        }
+        return asociado; //true si se agregó, false si no se agrego porque ya existía
     }
 
+    public boolean desasociar(Object dominio){
+        /* Elimina el par cuyo dominio coincida con el valor recibido por parámetro. Si lo encuentra y la
+        operación de eliminación termina con éxito devuelve verdadero y falso en caso contrario.*/
+        boolean desasociado = false;
+        int posicion = funcionHash(dominio);// Lugar del que se eliminaría el par de dominio
+        NodoHashMapeo actual = tabla[posicion]; // Nodo que estamos evaluando actualmente
+        NodoHashMapeo anterior = null; //Nodo que sirve de referencia para eliminar nodo actual si es el que buscamos borrar
+        
+        while(actual != null && !desasociado){
+            if((actual.getDominio()).equals(dominio)){// si es el que buscamos eliminar
+                if(anterior == null){
+                    tabla[posicion] = actual.getEnlace();//si es el primero de la lista
+                }else{
+                    anterior.setEnlace(actual.getEnlace());//si está en el medio o al final
+                }
+                cant--;
+                desasociado = true;
+            }
+            anterior = actual; //nodo que sera anterior al siguiente
+            actual = actual.getEnlace();
+        }
+        return desasociado;
+    }
 
+    public Object obtenerValor(Object dominio){
+        /*si en el mapeo se encuentra almacenado algún par cuyo dominio es valorDominio, esta operación
+        devuelve el valor de rango asociado a él. Precondición: valorDominio está en el mapeo (si no existe,
+        no se puede asegurar el funcionamiento de la operación).*/
+        Object rango = null;// valor buscado
+        int posicion = funcionHash(dominio);//ubicación del nodo buscado
+        NodoHashMapeo actual = tabla[posicion]; //Nodo que estamos evaluando actualmente
+        
+        while(actual != null){
+            if(actual.getDominio().equals(dominio)){
+                rango = actual.getRango(); //obtengo el valor buscado
+                actual = null; //finalizo el while
+            }else{
+                actual = actual.getEnlace();
+            }
 
-    private int funcionHash(ClaveCliente claveCliente){
+        }
+        return rango;
+    }
+
+    public Lista obtenerConjuntoDominio(){
+        // devuelve una lista con todos los valores de tipo dominio almacenados en el mapeo.
+        int cantAgregados = 0, posicion=0;//posicion = pos actual en la tabla
+        Lista lista = new Lista();
+        NodoHashMapeo nodoActual = null;
+
+        while(cantAgregados < cant && posicion<TAMANO){
+        //itera mientras el limite sea menor a la cantidad de elementos en la tabla, para que no recorra toda la tabla si ya no quedan más elementos
+            nodoActual=tabla[posicion];
+            while(nodoActual!=null){
+                lista.insertar(nodoActual.getDominio(),cantAgregados+1);
+                cantAgregados++;//se suma un agregado a la table
+                nodoActual = nodoActual.getEnlace();
+            }
+            posicion++;
+        }
+        return lista;
+    }
+    
+    public Lista obtenerConjuntoRango(){
+        //devuelve un conjunto con la unión de todos los valores de tipo rango almacenados en el mapeo.
+        int cantAgregados = 0, posicion=0;//posicion = pos actual en la tabla
+        Lista lista = new Lista();
+        NodoHashMapeo nodoActual = null;
+
+        while(cantAgregados < cant && posicion<TAMANO){
+        //itera mientras el limite sea menor a la cantidad de elementos en la tabla, para que no recorra toda la tabla si ya no quedan más elementos
+            nodoActual=tabla[posicion];
+            while(nodoActual!=null){
+                lista.insertar(nodoActual.getRango(),cantAgregados+1);
+                cantAgregados++;//se suma un agregado a la table
+                nodoActual = nodoActual.getEnlace();
+            }
+            posicion++;
+        }
+        return lista;
+    }
+
+    public boolean esVacio(){
+        //devuelve falso si hay al menos un par cargado en el mapeo y verdadero en caso contrario.
+        return cant==0;
+    }
+    
+    private int funcionHash(Object dominio){
         //Se opera con clave tipo cadena
-        int posicion = 0;
-        String cadena = claveCliente.getClaveString();
-
-        return posicion;
+        return 0;
     }
 }
 
--- TDAs/NodoHashMapeo.java
+++ TDAs/NodoHashMapeo.java
@@ -1,31 +1,28 @@
 package TDAs;
 
-import Mudanzas.Object;
-import Mudanzas.DatosCliente;
-
 public class NodoHashMapeo {
     private Object dominio;
-    private DatosCliente rango;
+    private Object rango;
     private NodoHashMapeo enlace;
     //Es igual a Nodo pero en vez de Elem posee dominio y rango
-    public NodoHashMapeo(Object Object, DatosCliente datosCliente, NodoHashMapeo enlace){
-        this.dominio = Object;
-        this.rango = datosCliente;
+    public NodoHashMapeo(Object dominio, Object rango, NodoHashMapeo enlace){
+        this.dominio = dominio;
+        this.rango = rango;
         this.enlace = enlace;
     }
     //Getters
     public Object getDominio(){
         return this.dominio;
     }
-    public DatosCliente getRango(){
+    public Object getRango(){
         return this.rango;
     }
     public NodoHashMapeo getEnlace(){
         return this.enlace;
     }
     //Setter
-    public void setRango(DatosCliente datosCliente){
-        this.rango = datosCliente;
+    public void setRango(Object rango){
+        this.rango = rango;
     }
     public void setEnlace(NodoHashMapeo enlace){
         this.enlace = enlace;

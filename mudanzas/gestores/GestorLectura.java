cebc96c8646c8d3ab1390db27b0cc088394742b0
TiagoZingoni <tiago.zingoni@est.fi.uncoma.edu.ar>
2026-04-29 19:00:12 -0300

    siguiendo gestor lectura

--- mudanzas/gestores/GestorLectura.java
+++ mudanzas/gestores/GestorLectura.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.util.StringTokenizer;
+import mudanzas.Ciudad;
+import mudanzas.ClaveCliente;
+import mudanzas.DatosCliente;
 
 public class GestorLectura {
 
@@ -37,14 +40,63 @@ public class GestorLectura {
                 //Mientras sigan habiendo strings
                 stActual = st.nextToken();//Le cargamos el string actual
                 switch (stActual) {
-                    case "S":
+                    case "C":
+                        int intCodPostal;
+                        String codPostal,
+                         nombreCiudad,
+                         nombreProvincia;
                         //Si es una Ciudad
-                        while (st.hasMoreTokens()) {
-                            //Para cada elemento del string
-                            stActual = st.nextToken();//Siguiente elemnto de la lista:
-
-                        }
+                        codPostal = st.nextToken();//Siguiente elemnto de la lista
+                        intCodPostal = Integer.parseInt(codPostal);//se pasa a int
+                        nombreCiudad = st.nextToken();
+                        nombreProvincia = st.nextToken();
+                        gestorCiudades.altaCiudad(new Ciudad(intCodPostal, nombreCiudad, nombreProvincia));
+                        gestorRutas.altaCiudad(intCodPostal);
                         break;
+                    case "P":
+                        //Si es una Persona
+                        int nroDoc,
+                         tel;
+                        String tipoDoc,
+                         nroDocSt,
+                         nombre,
+                         apellido,
+                         telSt,
+                         email;
+                        tipoDoc = st.nextToken();//Siguiente elemnto de la lista
+                        nroDocSt = st.nextToken();
+                        apellido = st.nextToken();
+                        nombre = st.nextToken();
+                        telSt = st.nextToken();
+                        email = st.nextToken();
+                        nroDoc = Integer.parseInt(nroDocSt);
+                        tel = Integer.parseInt(telSt);
+                        gestorClientes.altaCliente(new ClaveCliente(tipoDoc, nroDoc), new DatosCliente(nombre, apellido, tel, email));
+                        break;
+                    case "R":
+                        //Si es una Ruta
+                        int codPostalSalida,
+                         codPostalLlegada;
+                        String codPostalSalidaSt,
+                         codPostalLlegadaSt,
+                         distanciaSt;
+                        double distancia;
+                        codPostalSalidaSt = st.nextToken();
+                        codPostalLlegadaSt = st.nextToken();
+                        distanciaSt = st.nextToken();
+                        codPostalSalida = Integer.parseInt(codPostalSalidaSt);
+                        codPostalLlegada = Integer.parseInt(codPostalLlegadaSt);
+                        distancia = Double.parseDouble(distanciaSt);
+                        gestorRutas.altaRuta(codPostalSalida, codPostalLlegada, distancia);
+                        break;
+                    case "S":
+                        //Si es una Solicitud
+
+                        break;
+                    default:
+                        //Otro caso no hace nada
+                        break;
+
                 }
 
             }
